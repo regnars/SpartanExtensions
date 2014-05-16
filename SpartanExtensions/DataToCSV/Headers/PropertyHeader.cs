daa310539120c3f3b1f6e026d823a02ccc3063d8
istokis <ivars.stokis@ebit.lv>
2014-05-16 09:02:29 +0300

    Pielikts DataToCSV.

---
+++ SpartanExtensions/DataToCSV/Headers/PropertyHeader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace SpartanExtensions
+{
+    internal class PropertyHeader<T> : HeaderBase<T>
+    {
+        private PropertyInfo _propertyInfo;        
+
+        public PropertyHeader(PropertyInfo propertyInfo)
+            :base(propertyInfo.Name)
+        {
+            this._propertyInfo = propertyInfo;
+        }
+
+        internal override object GetValue(T dataItem)
+        {
+            return _propertyInfo.GetValue(dataItem, null);
+        }
+    }
+}
