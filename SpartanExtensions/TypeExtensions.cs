a7c8cccd167a21e9a22ee377a3cadb28c91204e0
Regnārs Breikšs <rbreikss@gmail.com>
2015-07-10 14:28:06 +0300

    GetAttribute method added.

--- SpartanExtensions/TypeExtensions.cs
+++ SpartanExtensions/TypeExtensions.cs
@@ -17,5 +17,21 @@ namespace SpartanExtensions
             return assemblyLocation.Replace(fileName, string.Empty);
             // ReSharper restore PossibleNullReferenceException
         }
+
+        /// <summary>
+        /// Gets an attribute corresponding to the type
+        /// </summary>
+        /// <param name="type"></param>
+        public static TAttribute GetAttribute<TAttribute>(this Type type)
+            where TAttribute : Attribute
+        {
+            var attributeType = typeof(TAttribute);
+            var attribute = (TAttribute)Attribute.GetCustomAttribute(type, attributeType);
+
+            if(attribute == null)
+                throw new Exception(string.Format("Can't find an attribute of a given the type \"{0}\" for the type \"{1}\".", attributeType.FullName, type.FullName));
+
+            return attribute;
+        }
     }
 }
